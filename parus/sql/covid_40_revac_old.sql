a444ff6d944ef183b5f690fae36f390daeef659e
Ubuntu <oleg.medovikov@mail.ru>
2022-09-11 07:18:37 +0000

    change covid_40 sql by script

--- parus/sql/covid_40_revac_old.sql
+++ parus/sql/covid_40_revac_old.sql
@@ -34,7 +34,7 @@ SELECT 1 AS INDX, ORGANIZATION,'Всего' as typevacine, concat(ORGANIZATION, 
 			INNER JOIN PARUS.BALANCEINDEXES bi 
 			on(d.BALANCEINDEX = bi.RN)
 		WHERE rf.code = '40 COVID 19'
-			and  r.BDATE =  trunc(SYSDATE) - 2
+			and r.BDATE in (trunc(SYSDATE) - 2, '2022-08-30')
 			and bi.CODE in ('revac_02_01_s','revac_03_01_s','revac_04_01_s','revac_05_01_s','revac_06_01_s','revac_07_01_s','revac_08_01_s'
 							,'revac_09_01_s','revac_10_01_s','revac_11_01_s','revac_12_01_s','revac_13_01_s','revac_14_01_s','revac_15_01_s'
 							,'revac_16_01_s','revac_17_01_s','revac_18_01_s','revac_19_01_s')
@@ -84,7 +84,7 @@ SELECT 2 AS INDX, ORGANIZATION,'Гам-КОВИД-Вак (Спутник-V)' as 
 			INNER JOIN PARUS.BALANCEINDEXES bi 
 			on(d.BALANCEINDEX = bi.RN)
 		WHERE rf.code = '40 COVID 19'
-			and  r.BDATE =  trunc(SYSDATE) - 2
+			and r.BDATE in (trunc(SYSDATE) - 2, '2022-08-30')
 			and bi.CODE in ('revac_02_02_s','revac_03_02_s','revac_04_02_s','revac_05_02_s','revac_06_02_s','revac_07_02_s','revac_08_02_s'
 							,'revac_09_02_s','revac_10_02_s','revac_11_02_s','revac_12_02_s','revac_13_02_s','revac_14_02_s','revac_15_02_s'
 							,'revac_16_02_s','revac_17_02_s','revac_18_02_s','revac_19_02_s')
@@ -134,7 +134,7 @@ SELECT 3 AS INDX,ORGANIZATION,'КовиВак' as typevacine, concat(ORGANIZATIO
 			INNER JOIN PARUS.BALANCEINDEXES bi 
 			on(d.BALANCEINDEX = bi.RN)
 		WHERE rf.code = '40 COVID 19'
-			and  r.BDATE =  trunc(SYSDATE) - 2
+			and r.BDATE in (trunc(SYSDATE) - 2, '2022-08-30')
 			and bi.CODE in ('revac_02_03_s','revac_03_03_s','revac_04_03_s','revac_05_03_s','revac_06_03_s','revac_07_03_s','revac_08_03_s'
 							,'revac_09_03_s','revac_10_03_s','revac_11_03_s','revac_12_03_s','revac_13_03_s','revac_14_03_s','revac_15_03_s'
 							,'revac_16_03_s','revac_17_03_s','revac_18_03_s','revac_19_03_s')
@@ -184,7 +184,7 @@ SELECT 4 AS INDX,ORGANIZATION,'ЭпиВакКорона' as typevacine, concat(O
 			INNER JOIN PARUS.BALANCEINDEXES bi 
 			on(d.BALANCEINDEX = bi.RN)
 		WHERE rf.code = '40 COVID 19'
-			and  r.BDATE =  trunc(SYSDATE) - 2
+			and r.BDATE in (trunc(SYSDATE) - 2, '2022-08-30')
 			and bi.CODE in ('revac_02_04_s','revac_03_04_s','revac_04_04_s','revac_05_04_s','revac_06_04_s','revac_07_04_s','revac_08_04_s'
 							,'revac_09_04_s','revac_10_04_s','revac_11_04_s','revac_12_04_s','revac_13_04_s','revac_14_04_s','revac_15_04_s'
 							,'revac_16_04_s','revac_17_04_s','revac_18_04_s','revac_19_04_s')
@@ -233,7 +233,7 @@ SELECT 5 AS INDX, ORGANIZATION,'Спутник Лайт' as typevacine, concat(O
 			INNER JOIN PARUS.BALANCEINDEXES bi 
 			on(d.BALANCEINDEX = bi.RN)
 		WHERE rf.code = '40 COVID 19'
-			and  r.BDATE =  trunc(SYSDATE) - 2
+			and r.BDATE in (trunc(SYSDATE) - 2, '2022-08-30')
 			and bi.CODE in ('revac_02_05_s','revac_03_05_s','revac_04_05_s','revac_05_05_s','revac_06_05_s','revac_07_05_s','revac_08_05_s'
 							,'revac_09_05_s','revac_10_05_s','revac_11_05_s','revac_12_05_s','revac_13_05_s','revac_14_05_s','revac_15_05_s'
 							,'revac_16_05_s','revac_17_05_s','revac_18_05_s','revac_19_05_s')
