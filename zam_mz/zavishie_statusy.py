ca4ae259258bd8bc03597e253fd27110a02a5b0a
oleg-medovikov <oleg.medovikov@mail.ru>
2022-11-14 16:53:01 +0300

    исправил зависшие статусы

--- zam_mz/zavishie_statusy.py
+++ zam_mz/zavishie_statusy.py
@@ -3,7 +3,7 @@ from .put_svod_for_mo  import put_svod_for_mo
 from .put_excel_for_mo import put_excel_for_mo
 
 def zavishie_statusy():
-    SQL_1 = open('zam_mz/sql/bez_ambulat_level_amb.sql','r').read()
+    SQL_1 = open('zam_mz/sql/zavishie_status.sql','r').read()
     SQL_2 = open('zam_mz/sql/bez_ambulat_level_noMO.sql','r').read()
 
     DF_1 = covid_sql( SQL_1 )
