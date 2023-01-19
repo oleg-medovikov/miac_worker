50a3e302136dec5613b395601572bce70de8587f
oleg-medovikov <oleg.medovikov@mail.ru>
2023-01-19 10:47:57 +0300

    change parus __init__

--- parus/__init__.py
+++ parus/__init__.py
@@ -1,27 +1,57 @@
-from .svod_4_2_covid_19      import svod_4_2_covid_19
-from .svod_4_3_covid_19      import svod_4_3_covid_19
-from .svod_26_covid_19       import svod_26_covid_19
-from .svod_27_covid_19       import svod_27_covid_19
+from .svod_4_2_covid_19 import svod_4_2_covid_19
+from .svod_4_3_covid_19 import svod_4_3_covid_19
+from .svod_26_covid_19 import svod_26_covid_19
+from .svod_27_covid_19 import svod_27_covid_19
 from .svod_27_covid_19_regiz import svod_27_covid_19_regiz
-from .svod_27_covid_small    import svod_27_covid_small
-from .svod_28_covid_19       import svod_28_covid_19
-from .svod_29_covid_19       import svod_29_covid_19
-from .svod_33_covid_19       import svod_33_covid_19
-from .svod_36_covid_19       import svod_36_covid_19
-from .svod_37_covid_19       import svod_37_covid_19
-from .svod_38_covid_19       import svod_38_covid_19
-from .svod_40_covid_19       import svod_40_covid_19
+from .svod_27_covid_small import svod_27_covid_small
+from .svod_28_covid_19 import svod_28_covid_19
+from .svod_29_covid_19 import svod_29_covid_19
+from .svod_33_covid_19 import svod_33_covid_19
+from .svod_36_covid_19 import svod_36_covid_19
+from .svod_37_covid_19 import svod_37_covid_19
+from .svod_38_covid_19 import svod_38_covid_19
+from .svod_40_covid_19 import svod_40_covid_19
 from .svod_40_covid_19_dates import svod_40_covid_19_dates
-from .svod_43_covid_19       import svod_43_covid_19
-from .svod_50_covid_19       import svod_50_covid_19
-from .svod_51_covid_19       import svod_51_covid_19
-from .svod_52_covid_19       import svod_52_covid_19
-from .svod_53_covid_19       import svod_53_covid_19
-from .svod_54_covid_19       import svod_54_covid_19
-from .svod_54_covid_new2     import svod_54_covid_new2
-from .extra_izv              import extra_izv
-from .distant_consult        import distant_consult
-from .evulshed               import evulshed
-from .mp_travm_v_hode_co     import mp_travm_v_hode_co
-from .laboratory             import laboratory
+from .svod_43_covid_19 import svod_43_covid_19
+from .svod_50_covid_19 import svod_50_covid_19
+from .svod_51_covid_19 import svod_51_covid_19
+from .svod_52_covid_19 import svod_52_covid_19
+from .svod_53_covid_19 import svod_53_covid_19
+from .svod_54_covid_19 import svod_54_covid_19
+from .svod_54_covid_new2 import svod_54_covid_new2
+from .extra_izv import extra_izv
+from .distant_consult import distant_consult
+from .evulshed import evulshed
+from .mp_travm_v_hode_co import mp_travm_v_hode_co
+from .laboratory import laboratory
 from .grip_and_ori import grip_and_ori
+
+__all__ = [
+    'svod_4_2_covid_19',
+    'svod_4_3_covid_19',
+    'svod_26_covid_19',
+    'svod_27_covid_19',
+    'svod_27_covid_19_regiz',
+    'svod_27_covid_small',
+    'svod_28_covid_19',
+    'svod_29_covid_19',
+    'svod_33_covid_19',
+    'svod_36_covid_19',
+    'svod_37_covid_19',
+    'svod_38_covid_19',
+    'svod_40_covid_19',
+    'svod_40_covid_19_dates',
+    'svod_43_covid_19',
+    'svod_50_covid_19',
+    'svod_51_covid_19',
+    'svod_52_covid_19',
+    'svod_53_covid_19',
+    'svod_54_covid_19',
+    'svod_54_covid_new2',
+    'extra_izv',
+    'distant_consult',
+    'evulshed',
+    'mp_travm_v_hode_co',
+    'laboratory',
+    'grip_and_ori',
+    ]
