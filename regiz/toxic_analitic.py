7d123ed1777d90206fda19bb0db8e99ac9898bc9
oleg-medovikov <oleg.medovikov@mail.ru>
2023-03-09 19:36:05 +0300

    add toxic_columns

--- regiz/toxic_analitic.py
+++ regiz/toxic_analitic.py
@@ -1,9 +1,9 @@
-import pandas as pd
 from datetime import datetime
 
 from system import return_mounth, write_styling_excel_file
 from .toxic_get_cases import toxic_get_cases
 from .toxic_checker import toxic_checker
+from .toxic_columns import toxic_columns
 
 
 class my_except(Exception):
@@ -24,55 +24,15 @@ def toxic_analitic(ARG):
     error = toxic_checker(DF)
     if len(error):
         DF = DF.loc[~DF['history_number'].isin(error['history_number'])]
+        error = toxic_columns(error)
+        error = error.fillna('ПУСТО!!!')
 
-    D = pd.DataFrame()
-    E = pd.DataFrame()
+    DF = toxic_columns(DF)
+    DF = DF.fillna('ПУСТО!!!')
 
-    DICT = {
-        'history_number': 'Номер истории болезни',
-        'date_aff_first': 'Дата открытия СМО',
-        'age': 'Возраст',
-        'gender': 'Пол',
-        'diagnosis': 'Диагноз',
-        'smo_fio': 'ФИО врача СМО',
-        'meddoc_fio': 'ФИО врача МД',
-        '303':  '303 - Дата установления диагноза',
-        '1101': '1101 - Место происшествия',
-        '1102': '1102 - Адрес места происшествия',
-        '1103': '1103 - Наименование места происшествия',
-        '1104': '1104 - Дата отравления',
-        '1105': '1105 - Дата первичного обращения',
-        '1106': '1106 - Токсичные вещества, часто встречающиеся',
-        '1107': '1107 - Токсичные вещества',
-        '1108': '1108 - Сочетание с алкоголем',
-        '1109': '1109 - Лицо, установившее диагноз',
-        '1110': '1110 - Оказана медицинская помощь',
-        '1111': '1111 - Место наступления смерти',
-        '1113': '1113 - Характер отравления',
-        '1114': '1114 - Количество отравившихся',
-        '1115': '1115 - Обстоятельства отравления',
-        '1116': '1116 - Обстоятельства отравления текст',
-        '1117': '1117 - Место приобретения яда',
-        '1118': '1118 - Место приобретения яда текст',
-        '1119': '1119 - Социальное положение',
-        '1123': '1123 - Район',
-        'Ошибка': 'Ошибка',
-    }
-
-    for key, value in DICT.items():
-        try:
-            E[value] = error[key]
-        except KeyError:
-            pass
-        try:
-            D[value] = DF[key]
-        except KeyError:
-            continue
-
-    D = D.fillna('ПУСТО!!!')
-    write_styling_excel_file(FILENAME, D, ARG.split(';')[1])
+    write_styling_excel_file(FILENAME, DF, ARG.split(';')[1])
     if len(error):
-        write_styling_excel_file(FILERROR, E, 'errors')
+        write_styling_excel_file(FILERROR, error, 'errors')
         return FILENAME + ';' + FILERROR
 
     return FILENAME
