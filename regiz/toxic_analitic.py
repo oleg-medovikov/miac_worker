49daa8ed6ec1c2cdf7d3824236ea051dfdf5409c
oleg-medovikov <oleg.medovikov@mail.ru>
2023-03-10 12:07:26 +0300

    add toxic_no_obs

--- regiz/toxic_analitic.py
+++ regiz/toxic_analitic.py
@@ -4,6 +4,7 @@ from system import return_mounth, write_styling_excel_file
 from .toxic_get_cases import toxic_get_cases
 from .toxic_checker import toxic_checker
 from .toxic_columns import toxic_columns
+from .toxic_no_obs import toxic_no_obs
 
 
 class my_except(Exception):
@@ -19,6 +20,7 @@ def toxic_analitic(ARG):
 
     FILENAME = f'temp/Аналитика по полноте регистра с {START} по {END}.xlsx'
     FILERROR = f'temp/Ошибки заполнения {START} по {END}.xlsx'
+    FILENOBS = f'temp/Нет показателей {START} по {END}.xlsx'
 
     # делаем проверку на ошибки
     error = toxic_checker(DF)
@@ -26,13 +28,21 @@ def toxic_analitic(ARG):
         DF = DF.loc[~DF['history_number'].isin(error['history_number'])]
         error = toxic_columns(error)
         error = error.fillna('ПУСТО!!!')
+    # проверяем на пац без показателей
+    no_obs = toxic_no_obs(START, END, ARG.split(';')[1])
 
     DF = toxic_columns(DF)
     DF = DF.fillna('ПУСТО!!!')
 
     write_styling_excel_file(FILENAME, DF, ARG.split(';')[1])
+    RETURN = FILENAME
+
     if len(error):
         write_styling_excel_file(FILERROR, error, 'errors')
-        return FILENAME + ';' + FILERROR
+        RETURN += ';' + FILERROR
 
-    return FILENAME
+    if len(no_obs):
+        write_styling_excel_file(FILENOBS, no_obs, 'errors')
+        RETURN += ';' + FILENOBS
+
+    return RETURN
---
+++ regiz/toxic_no_obs.py
@@ -0,0 +1,43 @@
+from pandas import DataFrame
+import requests
+
+from conf import REGIZ_AUTH
+
+DICT = {
+    'case_open_date': 'Дата заведения',
+    'history_number': 'Номер истории болезни',
+    'diagnosis': 'Диагноз',
+    'medical_help_name': 'Организация',
+    }
+
+
+class my_except(Exception):
+    pass
+
+
+def toxic_no_obs(START: str, END: str, ORG=None) -> 'DataFrame':
+    """токсикологические пациенты без показателей"""
+    URL = " https://regiz.gorzdrav.spb.ru/N3.BI/getDData" \
+        + f"?id=1177&args={START},{END}&auth={REGIZ_AUTH}"
+
+    try:
+        df = DataFrame(data=requests.get(URL).json())
+    except requests.Timeout:
+        return DataFrame()
+    except requests.ConnectionError:
+        return DataFrame()
+    else:
+        if not len(df):
+            return DataFrame()
+
+    if ORG is not None:
+        df = df.loc[df['medical_help_name'].isin([ORG])]
+
+    DF = DataFrame()
+    for key, value in DICT.items():
+        try:
+            DF[value] = df[key]
+        except KeyError:
+            continue
+
+    return DF
