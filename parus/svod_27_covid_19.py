c7688caf66412bba6aa9179cf2ed956b14d3bc7a
oleg-medovikov <oleg.medovikov@mail.ru>
2023-04-03 14:16:39 +0300

    update covid 27

--- parus/svod_27_covid_19.py
+++ parus/svod_27_covid_19.py
@@ -1,27 +1,30 @@
-import time, datetime, shutil, openpyxl
+from datetime import datetime
+import shutil
+import openpyxl
 from openpyxl.utils.dataframe import dataframe_to_rows
 from base import parus_sql
 
+
 def svod_27_covid_19():
-    SQL_1 = open('parus/sql/covid_27_svod.sql'    , 'r').read()
+    SQL_1 = open('parus/sql/covid_27_svod.sql', 'r').read()
     SQL_2 = open('parus/sql/covid_27_svod_old.sql', 'r').read()
-    SQL_3 = open('parus/sql/covid_27_svod_2.sql'  , 'r').read()
-    SQL_4 = open('parus/sql/covid_27_svod_3.sql'  , 'r').read()
-    SQL_5 = open('parus/sql/covid_27_svod_4.sql'  , 'r').read()
+    SQL_3 = open('parus/sql/covid_27_svod_2.sql', 'r').read()
+    SQL_4 = open('parus/sql/covid_27_svod_3.sql', 'r').read()
+    SQL_5 = open('parus/sql/covid_27_svod_4.sql', 'r').read()
 
-    DF      = parus_sql(SQL_1)
-    DF_OLD  = parus_sql(SQL_2)
-    DF_2    = parus_sql(SQL_3)
-    DF_3    = parus_sql(SQL_4)
-    DF_4    = parus_sql(SQL_5)
+    DF = parus_sql(SQL_1)
+    DF_OLD = parus_sql(SQL_2)
+    DF_2 = parus_sql(SQL_3)
+    DF_3 = parus_sql(SQL_4)
+    DF_4 = parus_sql(SQL_5)
 
-    DATE =  datetime.datetime.now().strftime('%d_%m_%Y')
+    DATE = datetime.now().strftime('%d_%m_%Y')
 
     DF_3 = DF_3.loc[~(DF_3['IDMO'].isnull())]
 
     for col in DF_3.columns[10:]:
         try:
-            DF_3[col] = DF_3[col].str.replace(',','.').astype(float)
+            DF_3[col] = DF_3[col].str.replace(',', '.').astype(float)
         except:
             pass
 
@@ -29,10 +32,9 @@ def svod_27_covid_19():
 
     for col in DF_4.columns[10:]:
         try:
-            DF_4[col] = DF_4[col].str.replace(',','.').astype(float)
+            DF_4[col] = DF_4[col].str.replace(',', '.').astype(float)
         except:
             pass
-
 
     NEW_NAME_1 = 'temp/' + DATE + '_1_27_COVID-19.xlsx'
     NEW_NAME_2 = 'temp/' + DATE + '_2_Результаты_исследований_материала_на_COVID-19.xlsx'
@@ -44,54 +46,47 @@ def svod_27_covid_19():
     shutil.copyfile('help/27_COVID_19_svod_3.xlsx', NEW_NAME_3)
     shutil.copyfile('help/27_COVID_19_svod_4.xlsx', NEW_NAME_4)
 
-    wb= openpyxl.load_workbook( NEW_NAME_1 )
-    ws = wb['Для заполнения']
-    
-    rows = dataframe_to_rows(DF,index=False, header=False)
-    for r_idx, row in enumerate(rows,4):  
-        for c_idx, value in enumerate(row, 1):
-            ws.cell(row=r_idx, column=c_idx, value=value)
-    
-    ws = wb['Пред.отч']
-    rows = dataframe_to_rows(DF_OLD,index=False, header=False)
-    for r_idx, row in enumerate(rows,4):  
-        for c_idx, value in enumerate(row, 1):
-            ws.cell(row=r_idx, column=c_idx, value=value)
-    
-    wb.save( NEW_NAME_1 )
+    wb = openpyxl.load_workbook(NEW_NAME_1)
 
+    DICT = {
+        'Для заполнения': (DF, 4, 1),
+        'Пред.отч': (DF_OLD, 4, 1),
+            }
 
-    wb= openpyxl.load_workbook( NEW_NAME_2 )
+    for key, value in DICT.items():
+        ws = wb[key]
+        rows = dataframe_to_rows(value[0], index=False, header=False)
+        for r_idx, row in enumerate(rows, value[1]):
+            for c_idx, val in enumerate(row, value[2]):
+                ws.cell(row=r_idx, column=c_idx, value=val)
+
+    wb.save(NEW_NAME_1)
+
+    wb = openpyxl.load_workbook(NEW_NAME_2)
     ws = wb['свод']
-    rows = dataframe_to_rows(DF_2,index=False, header=False)
-    for r_idx, row in enumerate(rows,4):  
+    rows = dataframe_to_rows(DF_2, index=False, header=False)
+    for r_idx, row in enumerate(rows, 4):
         for c_idx, value in enumerate(row, 1):
             ws.cell(row=r_idx, column=c_idx, value=value)
-    
-    wb.save( NEW_NAME_2 )
 
-    wb= openpyxl.load_workbook( NEW_NAME_3)
+    wb.save(NEW_NAME_2)
+
+    wb = openpyxl.load_workbook(NEW_NAME_3)
     ws = wb['свод']
-    rows = dataframe_to_rows(DF_3,index=False, header=False)
-    for r_idx, row in enumerate(rows,4):  
+    rows = dataframe_to_rows(DF_3, index=False, header=False)
+    for r_idx, row in enumerate(rows, 4):
         for c_idx, value in enumerate(row, 1):
             ws.cell(row=r_idx, column=c_idx, value=value)
-    
-    wb.save( NEW_NAME_3 )
 
+    wb.save(NEW_NAME_3)
 
-    wb= openpyxl.load_workbook( NEW_NAME_4)
+    wb = openpyxl.load_workbook(NEW_NAME_4)
     ws = wb['свод']
-    rows = dataframe_to_rows(DF_4,index=False, header=False)
-    for r_idx, row in enumerate(rows,4):  
+    rows = dataframe_to_rows(DF_4, index=False, header=False)
+    for r_idx, row in enumerate(rows, 4):
         for c_idx, value in enumerate(row, 1):
             ws.cell(row=r_idx, column=c_idx, value=value)
-    
-    wb.save( NEW_NAME_4 )
+
+    wb.save(NEW_NAME_4)
 
     return NEW_NAME_1 + ';' + NEW_NAME_2 + ';' + NEW_NAME_3 + ';' + NEW_NAME_4
-
-
-
-    
-
