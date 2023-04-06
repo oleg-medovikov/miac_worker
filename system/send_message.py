b04bdf46c890f467591a62ec7f6ba49d0bc38d0c
oleg-medovikov <oleg.medovikov@mail.ru>
2023-04-06 09:17:03 +0300

    add exception in send mess and file

--- system/send_message.py
+++ system/send_message.py
@@ -1,4 +1,7 @@
 import requests
+from requests.exceptions import ConnectionError
+from time import sleep
+
 from conf import TELEGRAM_API
 
 
@@ -11,7 +14,14 @@ def bot_send_text(mess: str, chat_id: int) -> bool:
         'parse_mode': 'Markdown',
         'text':        mess
         }
-    requests.get(send_text, data=data)
+    while True:
+        try:
+            requests.get(send_text, data=data)
+        except ConnectionError:
+            sleep(10)
+            continue
+        else:
+            break
 
     return True
 
@@ -22,4 +32,11 @@ def bot_send_file(file: str, chat_id: int) -> None:
         files = {'document': f}
 #        title = file.rsplit('/', 1)[-1]
         url = f'https://api.telegram.org/bot{TELEGRAM_API}/sendDocument'
-        requests.post(url, data={"chat_id": chat_id}, files=files)
+        while True:
+            try:
+                requests.post(url, data={"chat_id": chat_id}, files=files)
+            except ConnectionError:
+                sleep(10)
+                continue
+            else:
+                break
