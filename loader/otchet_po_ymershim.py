0cd46ff1e98cd00822c053e4a6e2d271f49831fc
oleg-medovikov <oleg.medovikov@mail.ru>
2023-05-19 17:06:33 +0300

    update ottchet po ymershim

--- loader/otchet_po_ymershim.py
+++ loader/otchet_po_ymershim.py
@@ -8,22 +8,35 @@ from base import covid_sql
 def otchet_po_ymershim():
     "отчёт по умершим почтой в понедельник"
 
-    SQL = "select * from [dbo].[View_Count_Dead_In_MO]"
+    SQL_1 = "select * from [dbo].[View_Count_Dead_In_MO]"
+    SQL_2 = "SELECT * FROM [COVID].[dbo].[v_Count_dead_Covid_Pnev_ORVI_Gripp]"
 
-    DF = covid_sql(SQL)
+    DF_1 = covid_sql(SQL_1)
+    DF_2 = covid_sql(SQL_2)
 
-    filename = 'temp/Умершие.xlsx'
+    filename_1 = 'temp/Умершие.xlsx'
 
-    DF.to_excel(filename, index=False)
+    DF_1.to_excel(filename_1, index=False)
 
     send_mail_with_excel(
             Dir.get('svetlichnay_mail'),
             'Отчёт по умершим',
             'Добрый день! Это отчёт от Олега',
-            filename)
+            filename_1)
+
+    filename_2 = 'temp/Умершие для РЗН.xlsx'
+
+    DF_2.to_excel(filename_2, index=False)
+
+    send_mail_with_excel(
+            Dir.get('svetlichnay_mail'),
+            'Отчёт по умершим',
+            'Добрый день! Это отчёт "умершие для РЗН" от Олега',
+            filename_2)
 
     try:
-        os.remove(filename)
+        os.remove(filename_1)
+        os.remove(filename_2)
     except FileNotFoundError:
         pass
     except FileExistsError:
