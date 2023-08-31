caa8079e1a1b663069e5f3a5e8e0c62576035cff
oleg-medovikov <oleg.medovikov@mail.ru>
2023-08-31 10:29:10 +0300

    update snils add death filter

--- zam_mz/sql/no_snils.sql
+++ zam_mz/sql/no_snils.sql
@@ -3,7 +3,8 @@ select f.УНРЗ,f.ФИО,f.[Дата рождения],f.[Медицинска
   from(
    select dbo.get_Gid(idPatient) as gid,[УНРЗ],[ФИО],[Дата рождения],[Медицинская организация]
       from robo.v_FedReg
-      where [СНИЛС] = 'Не идентифицирован' ) f
+      where [СНИЛС] = 'Не идентифицирован' and [Посмертный диагноз] = ''
+        ) f
   left join robo.snils_comment c
      on (f.УНРЗ = c.УНРЗ)
   left join (select dbo.get_Gid(idPatient) as gid  from  cv_umsrs) as um  on (f.gid = um.gid )
