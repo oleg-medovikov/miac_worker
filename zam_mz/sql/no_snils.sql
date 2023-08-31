b827833681a6fdf7a9db9e929f12cdd1763bc341
oleg-medovikov <oleg.medovikov@mail.ru>
2023-08-31 10:39:25 +0300

    update

--- zam_mz/sql/no_snils.sql
+++ zam_mz/sql/no_snils.sql
@@ -1,10 +1,9 @@
-select f.УНРЗ,f.ФИО,f.[Дата рождения],f.[Медицинская организация]
-,isnull(c.[Примечания],'') as [Примечания]
+select f.УНРЗ,f.ФИО,f.[Дата рождения],f.[Медицинская организация], isnull(c.[Примечания],'') as [Примечания]
   from(
    select dbo.get_Gid(idPatient) as gid,[УНРЗ],[ФИО],[Дата рождения],[Медицинская организация]
       from robo.v_FedReg
-      where [СНИЛС] = 'Не идентифицирован' and [Посмертный диагноз] = ''
-        ) f
+      where [СНИЛС] = 'Не идентифицирован' and [Исход заболевания] not in ('Смерть')
+	  ) f
   left join robo.snils_comment c
      on (f.УНРЗ = c.УНРЗ)
   left join (select dbo.get_Gid(idPatient) as gid  from  cv_umsrs) as um  on (f.gid = um.gid )
