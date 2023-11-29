540ac257d614b127d400466cbbac86e87c24a16f
oleg-medovikov <oleg.medovikov@mail.ru>
2023-11-29 10:34:31 +0300

    update class

--- clas/Command.py
+++ clas/Command.py
@@ -5,43 +5,36 @@ import requests
 
 
 class Command(BaseModel):
-    c_id:        int
-    c_category:  str
-    c_name:      str
+    c_id: int
+    c_category: str
+    c_name: str
     c_procedure: str
-    c_arg:       str
+    c_arg: str
     return_file: bool
-    asc_day:     bool
+    asc_day: bool
 
-    def add(self, USER_ID):
+    def add(self, USER_ID: int):
         "добавление новой команды"
-        HEADERS = dict(
-                KEY=TOKEN,
-                UID=str(USER_ID)
-            )
+        HEADERS = dict(KEY=TOKEN, UID=str(USER_ID))
         BODY = self.__dict__
-        URL = MIAC_API_URL + '/add_command'
+        URL = MIAC_API_URL + "/add_command"
         req = requests.post(URL, headers=HEADERS, json=BODY)
         return req.json()
 
-    def get_all(USER_ID):
+    @staticmethod
+    def get_all(USER_ID: int):
         "Получение всех комманд"
-        HEADERS = dict(
-                KEY=TOKEN,
-                UID=str(USER_ID)
-            )
-        URL = MIAC_API_URL + '/all_commands'
+        HEADERS = dict(KEY=TOKEN, UID=str(USER_ID))
+        URL = MIAC_API_URL + "/all_commands"
         req = requests.get(URL, headers=HEADERS)
         return req.json()
 
-    def get(C_ID):
+    @staticmethod
+    def get(C_ID: int):
         "Получение команды по айдишнику"
-        HEADERS = dict(
-                KEY=TOKEN,
-                CID=str(C_ID)
-            )
+        HEADERS = dict(KEY=TOKEN, CID=str(C_ID))
 
-        URL = MIAC_API_URL + '/get_command'
+        URL = MIAC_API_URL + "/get_command"
         req = requests.get(URL, headers=HEADERS)
-        if not req.json() is None:
+        if req.json() is not None:
             return Command(**req.json())
--- clas/Task.py
+++ clas/Task.py
@@ -12,65 +12,57 @@ class my_except(Exception):
 
 
 class Task(BaseModel):
-    t_id:         UUID = Field(default_factory=uuid4)
-    time_create:  datetime
-    client:       int
-    task_type:    str
-    c_id:         int
-    c_func:       str
-    c_arg:        str
-    users_list:   str
-    time_start:   Optional[datetime]
-    time_stop:    Optional[datetime]
-    comment:      Optional[str]
+    t_id: UUID = Field(default_factory=uuid4)
+    time_create: datetime
+    client: int
+    task_type: str
+    c_id: int
+    c_func: str
+    c_arg: str
+    users_list: str
+    time_start: Optional[datetime]
+    time_stop: Optional[datetime]
+    comment: Optional[str]
 
+    @staticmethod
     def get():
         """Взять доступную задачу"""
-        HEADERS = dict(
-            KEY = TOKEN
-                )
-        URL = MIAC_API_URL + '/get_task'
+        HEADERS = dict(KEY=TOKEN)
+        URL = MIAC_API_URL + "/get_task"
         req = requests.get(URL, headers=HEADERS)
-        if not req.json() is None:
+        if req.json() is not None:
             return Task(**req.json())
 
+    @staticmethod
     def restart():
         """Рестартануть выполнение задач, если бот перезапустился"""
-        HEADERS = dict(
-            KEY=TOKEN
-                )
-        URL = MIAC_API_URL + '/restart_tasks'
+        HEADERS = dict(KEY=TOKEN)
+        URL = MIAC_API_URL + "/restart_tasks"
         requests.post(URL, headers=HEADERS)
 
     def stop(self):
         """Закончить задачу"""
-        HEADERS = dict(
-            KEY=TOKEN
-                )
+        HEADERS = dict(KEY=TOKEN)
         BODY = self.__dict__
 
         try:
-            BODY['t_id'] = BODY['t_id'].hex
-        except:
-            pass
-        try:
-            BODY['time_create'] = BODY['time_create'].isoformat()
-        except:
-            pass
-        URL = MIAC_API_URL + '/stop_task'
+            BODY["t_id"] = BODY["t_id"].hex
+            BODY["time_create"] = BODY["time_create"].isoformat()
+        except Exception as e:
+            print(str(e))
+
+        URL = MIAC_API_URL + "/stop_task"
         requests.post(URL, headers=HEADERS, json=BODY)
 
     def users(self):
         """Получить список юзеров для рассылки"""
-        HEADERS = dict(
-            KEY=TOKEN
-                )
+        HEADERS = dict(KEY=TOKEN)
         BODY = self.__dict__
-        BODY['t_id'] = BODY['t_id'].hex
-        BODY['time_create'] = BODY['time_create'].isoformat()
-        #BODY['time_start']  = BODY['time_start'].isoformat()
+        BODY["t_id"] = BODY["t_id"].hex
+        BODY["time_create"] = BODY["time_create"].isoformat()
+        # BODY['time_start']  = BODY['time_start'].isoformat()
 
-        URL = MIAC_API_URL + '/get_task_users_list'
+        URL = MIAC_API_URL + "/get_task_users_list"
         req = requests.get(URL, headers=HEADERS, json=BODY)
 
         return req.json()
