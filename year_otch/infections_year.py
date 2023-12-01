85fb1297384e2d5dd612edf20f23da2c1de17717
oleg-medovikov <oleg.medovikov@mail.ru>
2023-12-01 15:20:00 +0300

    add infections_6

--- year_otch/infections_year.py
+++ year_otch/infections_year.py
@@ -5,6 +5,7 @@ from year_otch.dop._06_infections_2 import infections_2
 from year_otch.dop._07_infections_3 import infections_3
 from year_otch.dop._08_infections_4 import infections_4
 from year_otch.dop._09_infections_5 import infections_5
+from year_otch.dop._10_infections_6 import infections_6
 
 
 def infections_year():
@@ -15,6 +16,7 @@ def infections_year():
         "общ гепатит": df_processing(infections_3),
         "общ другие": df_processing(infections_4),
         "роддома ОКИ": df_processing(infections_5),
+        "роддома ВКИ": df_processing(infections_6),
     }
     write_excel(infections_1.name(), dict_)
     return infections_1.name()
