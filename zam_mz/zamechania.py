489bba275597e25fd24bb5a90e3f163c389147f9
oleg-medovikov <oleg.medovikov@mail.ru>
2023-12-05 12:32:43 +0300

    update bags zamechania

--- zam_mz/zamechania.py
+++ zam_mz/zamechania.py
@@ -1,18 +1,18 @@
 from base import covid_sql, covid_exec, covid_insert
 
-SQL_NAMES = [
-    ["net_diagnoz_covid.sql", "Не стоит диагноз ковид"],
-    ["no_snils.sql", "Без СНИЛСа"],
-    ["no_OMS.sql", "Нет сведений ОМС"],
-    ["bez_ishod.sql", "Без исхода заболевания больше 45 дней"],
-    ["no_lab.sql", "без лабораторного потверждения"],
-    ["net_dnevnik.sql", "Нет дневниковых записей"],
-    ["net_pad.sql", "Нет ПАД"],
-    ["neverni_vid_lecenia.sql", "Неверный вид лечения"],
-    ["bez_ambulat_level.sql", "Нет амбулаторного этапа"],
-    ["bez_ambulat_level_amb.sql", "Пациенты зависшие по МО"],
-    ["bez_ambulat_level_noMO.sql", "Пациенты зависшие без МО"],
-]
+SQL_NAMES = {
+    "net_diagnoz_covid.sql": "Не стоит диагноз ковид",
+    "no_snils.sql": "Без СНИЛСа",
+    "no_OMS.sql": "Нет сведений ОМС",
+    "bez_ishod.sql": "Без исхода заболевания больше 45 дней",
+    "no_lab.sql": "без лабораторного потверждения",
+    "net_dnevnik.sql": "Нет дневниковых записей",
+    "net_pad.sql": "Нет ПАД",
+    "neverni_vid_lecenia.sql": "Неверный вид лечения",
+    "bez_ambulat_level.sql": "Нет амбулаторного этапа",
+    "bez_ambulat_level_amb.sql": "Пациенты зависшие по МО",
+    "bez_ambulat_level_noMO.sql": "Пациенты зависшие без МО",
+}
 
 
 def zamechania():
@@ -30,19 +30,19 @@ def zamechania():
     del DF["Принадлежность"]
     DF["дата отчета"] = DATE
 
-    for FILE, NAME in SQL_NAMES:
+    for FILE, NAME in SQL_NAMES.items():
         SQL = open("zam_mz/sql/" + FILE, "r").read()
         PART = covid_sql(SQL)
         PART = PART.groupby(by=["Медицинская организация"], as_index=False).size()
 
         PART.rename(columns={"size": NAME}, inplace=True)
 
-    DF = DF.merge(
-        PART,
-        how="left",
-        left_on="Медицинская организация",
-        right_on="Медицинская организация",
-    )
+        DF = DF.merge(
+            PART,
+            how="left",
+            left_on="Медицинская организация",
+            right_on="Медицинская организация",
+        )
 
     SQL = f"""
     DELETE from [robo].[cv_Zamechania_fr]
@@ -50,9 +50,9 @@ def zamechania():
         """
 
     covid_exec(SQL)
-
     DF.fillna(0, inplace=True)
-
     covid_insert(DF, "cv_Zamechania_fr", "robo", False, "append")
 
-    return "Замечания сгенерированы"
+    filename = f"/tmp/zamechania_{DATE}.xlsx"
+    DF.to_excel(filename, index=False)
+    return filename
