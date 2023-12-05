c5635f98c3b58b9b51ecec3fbf30ca2753b7c000
oleg-medovikov <oleg.medovikov@mail.ru>
2023-12-05 15:20:00 +0300

    update zamechania file

--- zam_mz/zamechania_file.py
+++ zam_mz/zamechania_file.py
@@ -5,15 +5,15 @@ from openpyxl.utils.dataframe import dataframe_to_rows
 
 
 SQL_NAMES = [
-    ['no_snils.sql', 'Без СНИЛСа'],
-    ['no_OMS.sql', 'Нет сведений ОМС'],
-    ['bez_ishod.sql', 'Без исхода заболевания больше 30 дней'],
-    ['net_dnevnik.sql', 'Нет дневниковых записей'],
-    ['net_pad.sql', 'Нет ПАД'],
-    ['neverni_vid_lecenia.sql', 'Неверный вид лечения'],
-    ['bez_ambulat_level.sql', 'Нет амбулаторного этапа'],
-    ['bez_ambulat_level_amb.sql', 'Нет амбулаторного этапа (Амб.)'],
-    ['bez_ambulat_level_noMO.sql', 'Нет амбулаторного этапа (Без МО)']
+    ["no_snils.sql", "Без СНИЛСа"],
+    ["no_OMS.sql", "Нет сведений ОМС"],
+    ["bez_ishod.sql", "Без исхода заболевания больше 30 дней"],
+    ["net_dnevnik.sql", "Нет дневниковых записей"],
+    ["net_pad.sql", "Нет ПАД"],
+    ["neverni_vid_lecenia.sql", "Неверный вид лечения"],
+    ["bez_ambulat_level.sql", "Нет амбулаторного этапа"],
+    ["bez_ambulat_level_amb.sql", "Нет амбулаторного этапа (Амб.)"],
+    ["bez_ambulat_level_noMO.sql", "Нет амбулаторного этапа (Без МО)"],
 ]
 
 
@@ -24,150 +24,165 @@ def zamechania_file():
     """
     DATE = covid_sql(SQL).iat[0, 0]
 
-    SQL = open('zam_mz/sql/kolichestvo.sql', 'r').read()
+    SQL = open("zam_mz/sql/kolichestvo.sql", "r").read()
 
     DF = covid_sql(SQL)
+    DF = DF.loc[~DF["Тип организации"].isnull()]
 
     for FILE, NAME in SQL_NAMES:
-        SQL = open('zam_mz/sql/' + FILE, 'r').read()
+        SQL = open("zam_mz/sql/" + FILE, "r").read()
         PART = covid_sql(SQL)
-        PART = PART.groupby(
-                by=["Медицинская организация"],
-                as_index=False).size()
+        PART = PART.groupby(by=["Медицинская организация"], as_index=False).size()
 
         PART.rename(columns={"size": NAME}, inplace=True)
 
         DF = DF.merge(
-                PART,
-                how="left",
-                left_on='Медицинская организация',
-                right_on='Медицинская организация'
-            )
+            PART,
+            how="left",
+            left_on="Медицинская организация",
+            right_on="Медицинская организация",
+        )
 
     DF.fillna(0, inplace=True)
 
-    del DF['Уникальных пациентов']
-    DF = DF.loc[~(DF['Медицинская организация'] == 'МО другого региона')]
+    del DF["Уникальных пациентов"]
+    DF = DF.loc[~(DF["Медицинская организация"] == "МО другого региона")]
     DF.index = range(len(DF))
 
-    NEW_FILE = "temp/Замечания_за_" + DATE.strftime('%Y-%m-%d') + ".xlsx"
-    shutil.copyfile('help/Zamechania.xlsx', NEW_FILE)
+    NEW_FILE = "temp/Замечания_за_" + DATE.strftime("%Y-%m-%d") + ".xlsx"
+    shutil.copyfile("help/Zamechania.xlsx", NEW_FILE)
 
     wb = openpyxl.load_workbook(NEW_FILE)
 
-    ws = wb['main']
+    ws = wb["main"]
     PART = DF.copy()
-    del PART['Тип организации']
-    del PART['Принадлежность']
+    del PART["Тип организации"]
+    del PART["Принадлежность"]
     rows = dataframe_to_rows(PART, index=False, header=False)
     for r_idx, row in enumerate(rows, 5):
         for c_idx, value in enumerate(row, 7):
             ws.cell(row=r_idx, column=c_idx, value=value)
 
-    ws = wb['amb']
-    PART = DF.loc[DF['Тип организации'] == 'Амбулаторная'].copy()
-    del PART['Тип организации']
-    del PART['Принадлежность']
+    ws = wb["amb"]
+    PART = DF.loc[DF["Тип организации"] == "Амбулаторная"].copy()
+    del PART["Тип организации"]
+    del PART["Принадлежность"]
     rows = dataframe_to_rows(PART, index=False, header=False)
     for r_idx, row in enumerate(rows, 5):
         for c_idx, value in enumerate(row, 7):
             ws.cell(row=r_idx, column=c_idx, value=value)
 
-    ws = wb['amb_dist']
+    ws = wb["amb_dist"]
     PART = DF.loc[
-            (DF['Тип организации'] == 'Амбулаторная') \
-            & ~(DF['Принадлежность'].isin(['комитет здравоохранения', 'частные', 'федеральные']))
-            ].copy()
-    del PART['Тип организации']
+        (DF["Тип организации"] == "Амбулаторная")
+        & ~(
+            DF["Принадлежность"].isin(
+                ["комитет здравоохранения", "частные", "федеральные"]
+            )
+        )
+    ].copy()
+    del PART["Тип организации"]
     rows = dataframe_to_rows(PART, index=False, header=False)
     for r_idx, row in enumerate(rows, 5):
         for c_idx, value in enumerate(row, 7):
             ws.cell(row=r_idx, column=c_idx, value=value)
 
-    ws = wb['amb_kz']   
-    PART = DF.loc[(DF['Тип организации'] == 'Амбулаторная' )\
-            & ( DF['Принадлежность'].isin([
-                'комитет здравоохранения']) ) ].copy()
-    del PART ['Тип организации']
-    del PART ['Принадлежность']
+    ws = wb["amb_kz"]
+    PART = DF.loc[
+        (DF["Тип организации"] == "Амбулаторная")
+        & (DF["Принадлежность"].isin(["комитет здравоохранения"]))
+    ].copy()
+    del PART["Тип организации"]
+    del PART["Принадлежность"]
     rows = dataframe_to_rows(PART, index=False, header=False)
     for r_idx, row in enumerate(rows, 5):
         for c_idx, value in enumerate(row, 7):
             ws.cell(row=r_idx, column=c_idx, value=value)
 
-    ws = wb['amb_fed']   
-    PART = DF.loc[(DF['Тип организации'] == 'Амбулаторная' )\
-            & ( DF['Принадлежность'].isin([
-                'федеральные']) ) ].copy()
-    del PART ['Тип организации']
-    del PART ['Принадлежность']
-    rows = dataframe_to_rows(PART,index=False, header=False)
-    for r_idx, row in enumerate(rows,5):
+    ws = wb["amb_fed"]
+    PART = DF.loc[
+        (DF["Тип организации"] == "Амбулаторная")
+        & (DF["Принадлежность"].isin(["федеральные"]))
+    ].copy()
+    del PART["Тип организации"]
+    del PART["Принадлежность"]
+    rows = dataframe_to_rows(PART, index=False, header=False)
+    for r_idx, row in enumerate(rows, 5):
         for c_idx, value in enumerate(row, 7):
             ws.cell(row=r_idx, column=c_idx, value=value)
 
-    ws = wb['amb_ch']   
-    PART = DF.loc[(DF['Тип организации'] == 'Амбулаторная' )\
-            & ( DF['Принадлежность'].isin(['частные']) ) ].copy()
-    del PART ['Тип организации']
-    del PART ['Принадлежность']
-    rows = dataframe_to_rows(PART,index=False, header=False)
-    for r_idx, row in enumerate(rows,5):
+    ws = wb["amb_ch"]
+    PART = DF.loc[
+        (DF["Тип организации"] == "Амбулаторная")
+        & (DF["Принадлежность"].isin(["частные"]))
+    ].copy()
+    del PART["Тип организации"]
+    del PART["Принадлежность"]
+    rows = dataframe_to_rows(PART, index=False, header=False)
+    for r_idx, row in enumerate(rows, 5):
         for c_idx, value in enumerate(row, 7):
             ws.cell(row=r_idx, column=c_idx, value=value)
 
-    ws = wb['stat']   
-    PART = DF.loc[DF['Тип организации'] == 'Стационарная' ].copy()
-    del PART ['Тип организации']
-    del PART ['Принадлежность']
-    rows = dataframe_to_rows(PART,index=False, header=False)
-    for r_idx, row in enumerate(rows,5):
+    ws = wb["stat"]
+    PART = DF.loc[DF["Тип организации"] == "Стационарная"].copy()
+    del PART["Тип организации"]
+    del PART["Принадлежность"]
+    rows = dataframe_to_rows(PART, index=False, header=False)
+    for r_idx, row in enumerate(rows, 5):
         for c_idx, value in enumerate(row, 7):
             ws.cell(row=r_idx, column=c_idx, value=value)
 
-    ws = wb['stat_dist']   
-    PART = DF.loc[(DF['Тип организации'] == 'Стационарная' )\
-            & ~(DF['Принадлежность'].isin([
-                'комитет здравоохранения',
-                'частные','федеральные']) ) ].copy()
-    del PART ['Тип организации']
-    rows = dataframe_to_rows(PART,index=False, header=False)
-    for r_idx, row in enumerate(rows,5):
+    ws = wb["stat_dist"]
+    PART = DF.loc[
+        (DF["Тип организации"] == "Стационарная")
+        & ~(
+            DF["Принадлежность"].isin(
+                ["комитет здравоохранения", "частные", "федеральные"]
+            )
+        )
+    ].copy()
+    del PART["Тип организации"]
+    rows = dataframe_to_rows(PART, index=False, header=False)
+    for r_idx, row in enumerate(rows, 5):
         for c_idx, value in enumerate(row, 7):
             ws.cell(row=r_idx, column=c_idx, value=value)
 
-    ws = wb['stat_kz']   
-    PART = DF.loc[(DF['Тип организации'] == 'Стационарная' )\
-            & ( DF['Принадлежность'].isin([
-                'комитет здравоохранения']) )  ].copy()
-    del PART ['Тип организации']
-    del PART ['Принадлежность']
-    rows = dataframe_to_rows(PART,index=False, header=False)
-    for r_idx, row in enumerate(rows,5):
+    ws = wb["stat_kz"]
+    PART = DF.loc[
+        (DF["Тип организации"] == "Стационарная")
+        & (DF["Принадлежность"].isin(["комитет здравоохранения"]))
+    ].copy()
+    del PART["Тип организации"]
+    del PART["Принадлежность"]
+    rows = dataframe_to_rows(PART, index=False, header=False)
+    for r_idx, row in enumerate(rows, 5):
         for c_idx, value in enumerate(row, 7):
             ws.cell(row=r_idx, column=c_idx, value=value)
 
-    ws = wb['stat_fed']
-    PART = DF.loc[(DF['Тип организации'] == 'Стационарная' )\
-            &  (DF['Принадлежность'].isin([
-                'федеральные']) ) ].copy()
-    del PART ['Тип организации']
-    del PART ['Принадлежность']
-    rows = dataframe_to_rows(PART,index=False, header=False)
-    for r_idx, row in enumerate(rows,5):
+    ws = wb["stat_fed"]
+    PART = DF.loc[
+        (DF["Тип организации"] == "Стационарная")
+        & (DF["Принадлежность"].isin(["федеральные"]))
+    ].copy()
+    del PART["Тип организации"]
+    del PART["Принадлежность"]
+    rows = dataframe_to_rows(PART, index=False, header=False)
+    for r_idx, row in enumerate(rows, 5):
         for c_idx, value in enumerate(row, 7):
             ws.cell(row=r_idx, column=c_idx, value=value)
 
-    ws = wb['stat_ch']   
-    PART = DF.loc[(DF['Тип организации'] == 'Стационарная' )\
-            & ( DF['Принадлежность'].isin(['частные']) ) ].copy()
-    del PART ['Тип организации']
-    del PART ['Принадлежность']
-    rows = dataframe_to_rows(PART,index=False, header=False)
-    for r_idx, row in enumerate(rows,5):
+    ws = wb["stat_ch"]
+    PART = DF.loc[
+        (DF["Тип организации"] == "Стационарная")
+        & (DF["Принадлежность"].isin(["частные"]))
+    ].copy()
+    del PART["Тип организации"]
+    del PART["Принадлежность"]
+    rows = dataframe_to_rows(PART, index=False, header=False)
+    for r_idx, row in enumerate(rows, 5):
         for c_idx, value in enumerate(row, 7):
             ws.cell(row=r_idx, column=c_idx, value=value)
 
-    wb.save( NEW_FILE )
+    wb.save(NEW_FILE)
 
     return NEW_FILE
