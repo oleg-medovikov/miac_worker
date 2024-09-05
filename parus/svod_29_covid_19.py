4d3102c69cbcb6759e4052a81ba8582bf7b41157
oleg-medovikov <oleg.medovikov@mail.ru>
2024-09-05 14:28:48 +0300

    update 29 COVID 19 2

--- parus/svod_29_covid_19.py
+++ parus/svod_29_covid_19.py
@@ -1,19 +1,15 @@
-import time, datetime, shutil, openpyxl
+import shutil, openpyxl
 from openpyxl.utils.dataframe import dataframe_to_rows
 from base import parus_sql
 
 
 def svod_29_covid_19():
-    if int(time.strftime("%H")) <= 17:
-        SQL = open("parus/sql/covid_29_svod1.sql", "r").read()
-        DATE = datetime.datetime.now() - datetime.timedelta(days=1)
-    else:
-        SQL = open("parus/sql/covid_29_svod0.sql", "r").read()
-        DATE = datetime.datetime.now()
+    SQL = open("parus/sql/covid_29_svod0.sql", "r").read()
 
     df = parus_sql(SQL)
+    DATE = df["DAY"].unique()[0]
 
-    NEW_NAME = "temp/" + DATE.strftime("%d_%m_%Y") + "_29_COVID_19_cvod.xlsx"
+    NEW_NAME = f"temp/{DATE}_29_COVID_19_cvod.xlsx"
 
     shutil.copyfile("help/29_COVID_19_svod.xlsx", NEW_NAME)
 
